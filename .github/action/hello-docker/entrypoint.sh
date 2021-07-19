773e7b5e967e5ceeb81e3d377027d2a97da6d06c
rahulbist-devops <rahul.bist@bain.com>
2021-07-19 13:36:53 +0530

    Test 20

---
+++ .github/action/hello-docker/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [true]
+then
+    echo 'error'
+    exit 1
+fi
+
+echo "::debug ::Debug Message"
+echo "::warning ::Warning Message"
+echo "::error ::Error Message"
+
+echo "::add-mask::$1"
+echo "Hello $1"
+
+time=$(date)
+echo "::set-output name=time::$time"
+
+#echo "::set-env name=Hello::hello"
+
+
+
